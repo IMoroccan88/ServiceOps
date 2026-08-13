9d82319c7df2ac4a84c33da2f006a6fa9b0f177a
IMoroccan88 <alaehaji88@gmail.com>
2026-08-13 14:03:28 -0500

    creating the templates folder, and all the HTML files we need now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,15 +6,28 @@ app = Flask(__name__)
 # and send whatever home() returns back to them."
 @app.route("/")
 def home():
-    return "ServiceOps is running"
+    return render_template("index.html")
 
 
 # "When somebody requests /service-request, execute service_request(),
 # and send whatever service_request() returns back to them."
 @app.route("/service-request")
 def service_request():
-    return "Service request page is working!"
+    return render_template("service_request.html")
 
+
+# "When somebody requests /incidents, execute incidents(),
+# load incidents.html, and send that webpage back to them."
+@app.route("/incidents")
+def incidents():
+    return render_template("incidents.html")
+
+
+# "When somebody requests /health, execute health(),
+# load health.html, and send that webpage back to them."
+@app.route("/health")
+def health():
+    return render_template("health.html")
 
 # "If this file is being run directly, start the Flask web server."
 if __name__ == "__main__":
