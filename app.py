939169875e72b3e7eff5a47a46095ea5e179447b
IMoroccan88 <alaehaji88@gmail.com>
2026-08-17 14:49:48 -0500

    DataBase succesfully communicating with the Service request number

--- app.py
+++ app.py
@@ -3,10 +3,25 @@
 # and request so Python can read information sent by the browser."
 from flask import  (Flask, 
 render_template, 
-request
+request,
 )
+import psycopg
 
 app = Flask(__name__)
+
+# Create and return a connection to the ServiceOps database.
+def get_db_connection():
+    connection = psycopg.connect(
+        host="localhost",
+        dbname="serviceops",
+        user="postgres",
+        password="1A2l3a4e$",
+        port=5433
+    )
+
+    # Give the database connection back to whatever route called this function.
+    return connection
+
 
 # "When somebody requests /, execute home(),
 # and send whatever home() returns back to them."
@@ -28,7 +43,59 @@ def service_request():
 
         # If an SR number was entered, show what the customer searched for.
         if sr_number:
-            return f"You searched for Service Request: {sr_number}"
+            # Open a connection to the ServiceOps database.
+            connection = get_db_connection()
+
+            # Use the connection to create a cursor for running SQL.
+            cursor = connection.cursor()
+
+            # After Cursor calls the DB, enter the syntax that should take place.
+            cursor.execute(
+                """
+                SELECT service_requests.sr_number,
+                service_requests.issue,
+                customers.name,
+                companies.company_name
+                FROM service_requests
+
+                JOIN customers
+                ON service_requests.customer_id = customers.customer_id
+
+                JOIN companies
+                ON customers.company_id = companies.company_id
+
+                WHERE service_requests.sr_number = %s
+                """
+            ,
+            (sr_number,)
+            )
+
+            # and store it in the Python variable service_request.
+            service_request = cursor.fetchone()
+
+            # Close the cursor when we're finished using it.
+            cursor.close()
+
+            # Close the database connection when we're finished.
+            connection.close()
+
+
+            # "If PostgreSQL returned a Service Request
+            if service_request:
+
+                    return f"""
+                        SR Number: {service_request[0]}<br>
+                        Issue: {service_request[1]}<br>
+                        Customer: {service_request[2]}<br>
+                        Company: {service_request[3]}
+                        """
+
+            else:
+
+                    return "Requested Service Request does not exist."
+            
+            # Send a response back to the browser.
+            #return f"You searched for Service Request: {sr_number}" 
 
         # If no SR number was entered, simply display the Service Request page.
         else:
@@ -51,12 +118,12 @@ def service_request():
 
     # Temporarily display the submitted information
     # so we can prove Flask received everything correctly.
-    return f"""
-    Customer: {customer_name}<br>
-    Email: {email}<br>
-    Priority: {priority}<br>
-    Issue: {issue}
-    """
+        return f"""
+        Customer: {customer_name}<br>
+        Email: {email}<br>
+        Priority: {priority}<br>
+        Issue: {issue}
+        """
 
 # "When somebody requests /incidents, execute incidents(),
 # load incidents.html, and send that webpage back to them."
@@ -70,6 +137,7 @@ def incidents():
 @app.route("/health")
 def health():
     return render_template("health.html")
+
 
 # "If this file is being run directly, start the Flask web server."
 if __name__ == "__main__":
